4c65eeb71bdceab9bc016e17655603e7890707d5
robervip <44229332+robervip@users.noreply.github.com>
2019-03-12 00:47:35 +0100

    Add files via upload

---
+++ reguistro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="listadoUsuarios.php" method="post">
+        Dame tu nombre:
+        <input type="text" name="nombre" value="" idea id="nombre"><br><br>
+        Dime tu primer apellido:
+        <input type="text" name="apellido" value="" idea id="apellido"><br><br>
+        Dime un nombre de usuario:
+        <input type="text" name="usuario" value="" idea id="usuario">
+        <input type="submit" name="button" value="Enviar" onclick="comprobar()">
+    </form>
+    <script type="text/javascript" src="comprobar.js">
+    </script>
+  </body>
+</html>
